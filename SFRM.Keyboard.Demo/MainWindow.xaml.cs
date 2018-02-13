b685933631d95dc9dd8b86ef26a2a75c4b296f3c
Federico Fernandez <fede.fernandez.it@gmail.com>
2018-02-13 19:31:19 -0300

    Global KeyDown

--- SFRM.Keyboard.Demo/MainWindow.xaml.cs
+++ SFRM.Keyboard.Demo/MainWindow.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +23,65 @@ namespace SFRM.Keyboard.Demo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private LowLevelKeyboardProc hook = null;
+        private bool firstStroke = false;
+        private IntPtr hHook;
+        private string keyStored;
+
         public MainWindow()
         {
             InitializeComponent();
+            hook = new LowLevelKeyboardProc(MyCallbackFunction);
+            
+            // setup a keyboard hook            
+            using (Process process = Process.GetCurrentProcess())
+            using (ProcessModule module = process.MainModule)
+            {
+                IntPtr hModule = GetModuleHandle(module.ModuleName);
+                hHook = SetWindowsHookEx(WH_KEYBOARD_LL, hook, hModule, 0);
+            }            
         }
+    
+        private IntPtr MyCallbackFunction(int code, IntPtr wParam, IntPtr lParam)
+        {
+            if (code >= 0 && (IntPtr)WM_KEYDOWN == wParam) 
+            {
+                if (!firstStroke)
+                {
+                    firstStroke = !firstStroke;
+                    TextKeys.Text = string.Empty;
+                }                
+                int vkCode = Marshal.ReadInt32(lParam);
+                string vkString = ((Keys)vkCode).ToString();
+                keyStored += vkString;
+                TextKeys.Text = keyStored;                
+            }
+            return CallNextHookEx(hHook, code, wParam, lParam);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            UnhookWindowsHookEx(hHook);
+        }
+
+        #region DLLs Imported
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr SetWindowsHookEx(int hookType, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);        
+
+        delegate IntPtr LowLevelKeyboardProc(int code, IntPtr wParam, IntPtr lParam);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        #endregion        
     }
 }
