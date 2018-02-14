e0a82a5bcaebf55641e1256b27346ee1c9f4a2f0
Federico Fernandez <fede.fernandez.it@gmail.com>
2018-02-14 03:10:49 -0300

    Anula Hook anterior

--- SFRM.Keyboard.Demo/MainWindow.xaml.cs
+++ SFRM.Keyboard.Demo/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MessageBox = System.Windows.MessageBox;
+using Timer = System.Timers.Timer;
 
 namespace SFRM.Keyboard.Demo
 {
@@ -26,18 +29,26 @@ namespace SFRM.Keyboard.Demo
         private LowLevelKeyboardProc hook = null;
         private bool firstStroke = false;
         private IntPtr hHook;
+        private IntPtr hModule;
         private string keyStored;
+        private Timer farmaticListener;
+        private bool farmaticActived;
+        private WinEventDelegate winHook = null;
+        private IntPtr wHook;
+        private string lastWindowTitle;
+        private string currentWindowTitle;
 
         public MainWindow()
         {
-            InitializeComponent();
-            hook = new LowLevelKeyboardProc(MyCallbackFunction);
-            
-            // setup a keyboard hook                                    
-            IntPtr hModule = GetModuleHandle(null);
-            hHook = SetWindowsHookEx(WH_KEYBOARD_LL, hook, hModule, 0);            
+            InitializeComponent();            
+            hook = new LowLevelKeyboardProc(MyCallbackFunction);                                
+            hModule = GetModuleHandle(null);
+            hHook = SetWindowsHookEx(WH_KEYBOARD_LL, hook, hModule, 0);
+
+            winHook = new WinEventDelegate(WinEventProc);
+            wHook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, winHook, 0, 0, WINEVENT_OUTOFCONTEXT);            
         }
-    
+        
         private IntPtr MyCallbackFunction(int code, IntPtr wParam, IntPtr lParam)
         {
             if (code >= 0 && (IntPtr)WM_KEYDOWN == wParam) 
@@ -55,9 +66,36 @@ namespace SFRM.Keyboard.Demo
             return CallNextHookEx(hHook, code, wParam, lParam);
         }
 
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        private void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            var window = GetActiveWindowTitle();
+            if (window == "Keyboard Disable Demo")
+            {
+                Debug.WriteLine(window);
+                Thread.Sleep(150);
+                UnhookWindowsHookEx(hHook);
+                hHook = SetWindowsHookEx(WH_KEYBOARD_LL, hook, hModule, 0);
+            }            
+        }
+
         private void Window_Closed(object sender, EventArgs e)
         {
             UnhookWindowsHookEx(hHook);
+            UnhookWinEvent(wHook);
         }
 
         #region DLLs Imported
@@ -78,6 +116,23 @@ namespace SFRM.Keyboard.Demo
 
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+
+        [DllImport("user32.dll")]
+        static extern bool UnhookWinEvent(IntPtr hWinEventHook);        
         #endregion        
     }
 }
